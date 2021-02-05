aa1b9eaae578be1b0ad6ff91df2696cade65b088
Kajal Rana <P2514865@my365.dmu.ac.uk>
2021-02-05 17:35:31 +0000

    Added testing and class for Stock management

---
+++ Testing4/tstStock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AnStock = new clsStock();
+            //test to see that is exists
+            Assert.IsNotNull(AnStock);
+    
+        }
+    }
+}
