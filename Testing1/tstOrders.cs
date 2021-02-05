92b914e149ecc1aacf0580b292c21db3612e1b19
Mohmed-Ayaz Suleman <P2514862@my365.dmu.ac.uk>
2021-02-05 16:32:52 +0000

    Almost finished

--- Testing1/tstOrders.cs
+++ Testing1/tstOrders.cs
@@ -10,11 +10,24 @@ namespace Testing1
         [TestMethod]
         public void InstanceOK()
         {
-
             //create an instance of the class we want to create
             clsOrders AnOrder = new clsOrders();
+
             //test to see that it exists
             Assert.IsNotNull(AnOrder);
+
+        }
+        [TestMethod]
+        public void Over18PropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrders AnOrder = new clsOrders();
+            //some test data
+            Boolean TestData = true;
+            //assign the data to the property
+            AnOrder.Active = TestData;
+            //test to see if the two values are the same
+            Assert.AreEqual(AnOrder.Active, TestData);
         }
     }
 }
